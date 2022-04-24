30e2250211c71ace6d7176c462b9e6c8feaf7d64
juangallino <juangallino>
2022-04-24 12:27:28 -0300

    Creacion de main

---
+++ src/Dominio/SearchMain.java
@@ -0,0 +1,16 @@
+package Dominio;
+
+import frsf.cidisi.faia.exceptions.PrologConnectorException;
+import frsf.cidisi.faia.simulator.SearchBasedAgentSimulator;
+
+public class SearchMain {
+
+    public static void main(String[] args) throws PrologConnectorException {
+        Agente agente = new Agente();
+       Ambiente ambiente = new Ambiente();
+
+        SearchBasedAgentSimulator simulator = new SearchBasedAgentSimulator(ambiente, agente);
+
+        simulator.start();
+    }
+}
